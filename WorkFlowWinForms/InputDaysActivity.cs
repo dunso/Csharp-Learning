0d0b690451e1af11c5d656f8f43a2cce78d201af
dunso <admin@dun.so>
2016-11-05 15:40:53 +0800

    create

---
+++ WorkFlowWinForms/InputDaysActivity.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+
+namespace WorkFlowWinForms
+{
+    // stop the winform and write the information to the workflow
+    public sealed class InputDaysActivity : NativeActivity
+    {
+        // Define an activity input argument of type string
+        public InArgument<string> inBookMarkName { get; set; }
+
+        public OutArgument<int> outDays { get; set; }
+
+        public InOutArgument<string> inOutStr { get; set; }
+
+        protected override bool CanInduceIdle
+        {
+            get
+            {
+                return true;
+            }
+        }
+        // If your activity returns a value, derive from CodeActivity<TResult>
+        // and return the value from the Execute method.
+        protected override void Execute(NativeActivityContext context)
+        {
+            // Obtain the runtime value of the Text input argument
+            //string text = context.GetValue(this.Text);
+
+
+            //stop the workflow
+
+            //when have data , let the workflow continue
+            string bookMarkName = context.GetValue(inBookMarkName);
+
+            //create bookmark
+            Console.WriteLine("cretae bookmark");
+            context.CreateBookmark(bookMarkName,new BookmarkCallback(CallBackFun));
+
+
+            //int days = int.Parse(Console.ReadLine());
+            //context.SetValue(outDays, days);
+        }
+
+        //after bookmark,it will be callback
+        private void CallBackFun(NativeActivityContext context, Bookmark bookmark, object value)
+        {
+            context.SetValue(outDays, (int)value);
+            Console.WriteLine("bookmark continue");
+        }
+    }
+}
---
+++ workflow/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("workflow")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("workflow")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4bf78cbf-34a6-4ca2-820a-95845f10a83e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
