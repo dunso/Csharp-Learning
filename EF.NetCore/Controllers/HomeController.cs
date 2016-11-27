e3fc18f9ee069d61a0a96533c5fb44ffa03123c3
dunso <admin@dun.so>
2016-11-27 23:06:53 +0800

    update

--- EF.NetCore/Controllers/HomeController.cs
+++ EF.NetCore/Controllers/HomeController.cs
@@ -3,21 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using EF.NetCore.Models.University;
+using EF.NetCore.Models.SchoolViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace EF.NetCore.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<ActionResult> About()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Contact()
